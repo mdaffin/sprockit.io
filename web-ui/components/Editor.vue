fa80154d650361b2060dec580f6eb739a7b0e855
Michael Daffin <michael@daffin.io>
2019-10-12 21:07:08 +0100

    Fixes linting errors

--- web-ui/components/Editor.vue
+++ web-ui/components/Editor.vue
@@ -1,20 +1,22 @@
 <template>
   <client-only placeholder="Codemirror Loading...">
     <div>
-      <div class = "editor-panel-handle">
-        <div class = "editor-panel-handle-tab" v-bind:class="{ isSelected: tab === 'Script' }">
-          <span v-on:click="handleTab('Script')">Script</span>
+      <div class="editor-panel-handle">
+        <div
+          class="editor-panel-handle-tab"
+          :class="{ isSelected: tab === 'Script' }"
+        >
+          <span @click="handleTab('Script')">Script</span>
         </div>
-        <div class = "editor-panel-handle-tab" v-bind:class="{ isSelected: tab === 'Console' }">
-          <span v-on:click="handleTab('Console')">Console</span>
+        <div
+          class="editor-panel-handle-tab"
+          :class="{ isSelected: tab === 'Console' }"
+        >
+          <span @click="handleTab('Console')">Console</span>
         </div>
         <ExecuteButton />
       </div>
-      <codemirror 
-        v-model="code" 
-        :options="cmOption" 
-        @input="onCmCodeChange"
-      />
+      <codemirror v-model="code" :options="cmOption" @input="onCmCodeChange" />
     </div>
   </client-only>
 </template>
@@ -24,12 +26,12 @@ import ExecuteButton from "~/components/EditorComponents/ExecuteButton";
 
 export default {
   components: {
-    ExecuteButton
+    ExecuteButton,
   },
   data() {
     return {
       code: "const a = 10;\nconst b = 20;\nconsole.log(a + b);\n",
-      tab: 'Script',
+      tab: "Script",
       cmOption: {
         tabSize: 4,
         styleActiveLine: true,
@@ -50,53 +52,53 @@ export default {
     };
   },
   created() {
-    if(this.$bus)
-      this.$bus.emit('update-code', this.code);
+    if (this.$bus) this.$bus.emit("update-code", this.code);
   },
   methods: {
     onCmCodeChange(event) {
-      this.$bus.emit('update-code', event);
+      this.$bus.emit("update-code", event);
     },
     handleTab(tab) {
       this.tab = tab;
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style>
-.CodeMirror, .vue-codemirror {
+.CodeMirror,
+.vue-codemirror {
   height: 100%;
   width: 100%;
 }
 
-.editor-panel-handle{
+.editor-panel-handle {
   position: relative;
   right: 35px;
-  margin-right:-35px;
-  background-color:#2a3a4b;
+  margin-right: -35px;
+  background-color: #2a3a4b;
   cursor: e-resize;
 }
 
 .editor-panel-handle-tab {
-  width:35px;
-  height:100px;
-  background:#2979D3;
-  color:white;
-  display:flex;
+  width: 35px;
+  height: 100px;
+  background: #2979d3;
+  color: white;
+  display: flex;
   cursor: pointer;
 }
 
-.editor-panel-handle-tab span{
-  height:35px;
-  min-width:100px;
-  padding:5px;
-  text-align:center;
+.editor-panel-handle-tab span {
+  height: 35px;
+  min-width: 100px;
+  padding: 5px;
+  text-align: center;
   transform-origin: 0 0;
   transform: rotate(-90deg) translate(-100px, 0);
 }
 
 .isSelected {
-  background:purple;
+  background: purple;
 }
 </style>
--- web-ui/pages/index.vue
+++ web-ui/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="output" id="output-area">
       <p>script output will go here</p>
-      <iframe id = "output" />
+      <iframe id="output" />
     </div>
     <Editor class="editor-panel" />
   </div>
@@ -17,48 +17,48 @@ export default {
   },
   created() {
     //For some reason bus crashes so this if statement is neccessary
-    if(this.$bus) {
-      this.$bus.on('update-code', this.onCmCodeChange);
-      this.$bus.on('runCode', this.runCode);
+    if (this.$bus) {
+      this.$bus.on("update-code", this.onCmCodeChange);
+      this.$bus.on("runCode", this.runCode);
     }
   },
   beforeDestroy() {
-      this.$bus.off('update-code', this.onCmCodeChange);
-      this.$bus.off('runCode', this.runCode);
+    this.$bus.off("update-code", this.onCmCodeChange);
+    this.$bus.off("runCode", this.runCode);
   },
   data() {
     return {
-      code: ''
+      code: "",
     };
   },
   methods: {
     onCmCodeChange(event) {
       this.code = event;
     },
-    runCode(){
+    runCode() {
       let code = this.code;
 
       let code_norm = code.toLowerCase();
 
-      if (code_norm.indexOf('<script')==-1) {
-        code = `<script>${code}</s`+`cript>`;
+      if (code_norm.indexOf("<script") == -1) {
+        code = `<script>${code}</s` + `cript>`;
       }
 
       //Generate New Iframe
       document.getElementById("output").remove();
 
-      let i = document.createElement('IFRAME');
-      i.id = 'output';
+      let i = document.createElement("IFRAME");
+      i.id = "output";
 
-      let parent = document.getElementById('output-area');
+      let parent = document.getElementById("output-area");
       parent.appendChild(i);
 
-      var doc = document.getElementById('output').contentDocument;
+      var doc = document.getElementById("output").contentDocument;
       doc.open();
-      doc.write(code); 
+      doc.write(code);
       doc.close();
-    }
-  }
+    },
+  },
 };
 </script>
 
