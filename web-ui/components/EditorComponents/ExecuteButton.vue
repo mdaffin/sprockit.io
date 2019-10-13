95ece48617d95db3410e13b3bd3e1c666b75b820
Jody LeCompte <jody@jodylecompte.com>
2019-10-13 15:28:37 -0500

    Center run button text

--- web-ui/components/EditorComponents/ExecuteButton.vue
+++ web-ui/components/EditorComponents/ExecuteButton.vue
@@ -1,7 +1,5 @@
 <template>
-  <div @click="$emit('click')" class="executeButton">
-    Run
-  </div>
+  <div @click="$emit('click')" class="executeButton">Run</div>
 </template>
 
 <style scoped>
@@ -11,5 +9,8 @@
   color: white;
   background: green;
   cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
