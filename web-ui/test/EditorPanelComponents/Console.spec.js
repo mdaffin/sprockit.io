2d973f35386e108f99bdac7c682c997cdb719544
Jody LeCompte <jody@jodylecompte.com>
2019-10-17 15:49:26 -0500

    Add tests for Console component

---
+++ web-ui/test/EditorPanelComponents/Console.spec.js
@@ -0,0 +1,13 @@
+import { mount, shallowMount } from "@vue/test-utils";
+import Console from "@/components/EditorPanelComponents/Console";
+
+describe("Console", () => {
+  test("Displays console output when passed", () => {
+    const wrapper = shallowMount(Console);
+    wrapper.setData({ console: "This is some console output" });
+
+    const content = wrapper.find("#console").text();
+
+    expect(content).toEqual("This is some console output");
+  });
+});
