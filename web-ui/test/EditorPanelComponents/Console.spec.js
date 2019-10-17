e80b230b5d63a546c3d545a4cae393d0d934e020
Jody LeCompte <jody@jodylecompte.com>
2019-10-17 15:50:57 -0500

    Remove un-used import

--- web-ui/test/EditorPanelComponents/Console.spec.js
+++ web-ui/test/EditorPanelComponents/Console.spec.js
@@ -1,4 +1,4 @@
-import { mount, shallowMount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import Console from "@/components/EditorPanelComponents/Console";
 
 describe("Console", () => {
