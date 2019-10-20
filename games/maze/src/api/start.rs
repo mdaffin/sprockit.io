81deeaf0ff51d64ad4b57e1421366a4ae4b53ea1
Michael Daffin <michael@daffin.io>
2019-10-21 00:51:40 +0100

    Refactor the start endpoing to return HttpResponse

--- games/maze/src/api/start.rs
+++ games/maze/src/api/start.rs
@@ -1,18 +1,18 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::HttpResponse;
 use log::info;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 use super::{SessionToken, Sessions};
 use crate::maze::Maze;
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Response {
+    token: SessionToken,
+}
+
 /// The /start endpoint. Creates a new game session and returns the token used to idenfiy this
 /// session.
-pub fn start(state: Sessions) -> impl Responder {
-    #[derive(Debug, Serialize)]
-    struct Response {
-        token: SessionToken,
-    }
-
+pub fn start(state: Sessions) -> HttpResponse {
     let token = SessionToken::new();
     let maze = Maze::new(10);
 
@@ -27,30 +27,23 @@ pub fn start(state: Sessions) -> impl Responder {
 
 #[cfg(test)]
 mod tests {
-    use super::{super::routes, SessionToken, Sessions};
+    use super::{super::routes, Response, Sessions};
     use actix_web::{test, web, App};
-    use serde::Deserialize;
     use std::collections::HashMap;
     use std::sync::Mutex;
 
     #[test]
     /// Starting a new session adds the session to the store and returns the corresponding token
     fn new_session_stored_and_token_returned() {
-        #[derive(Debug, Deserialize)]
-        struct Response {
-            token: SessionToken,
-        }
-
         let sessions: Sessions = web::Data::new(Mutex::new(HashMap::new()));
         let mut app =
             test::init_service(App::new().register_data(sessions.clone()).configure(routes));
         let req = test::TestRequest::post().uri("/start").to_request();
 
-        let response: HashMap<String, SessionToken> = test::read_response_json(&mut app, req);
+        let response: Response = test::read_response_json(&mut app, req);
 
-        assert!(response.get("token").is_some());
         assert_eq!(
-            response.get("token"),
+            Some(&response.token),
             sessions.lock().unwrap().keys().next()
         );
     }
