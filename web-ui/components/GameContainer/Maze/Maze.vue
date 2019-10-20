c191079568f3ae91e50b2dc0225713c4356b29e8
fsterneder <16085695+fsterneder@users.noreply.github.com>
2019-10-20 17:18:04 +0200

    shows an ascii map fix #33 (#60)
    * prettier console output
    
    * deleted unused default files
    
    * Revert "deleted unused default files"
    
    This reverts commit 3ee65c5ca444ab81acfdb684e6757f5128c9f0e4.
    
    * display (hard-coded) map
    
    * passing a key
    
    * less whitespace
    
    * only one pre
    
    * changed symbols
    
    * unused css gone

--- web-ui/components/GameContainer/Maze/Maze.vue
+++ web-ui/components/GameContainer/Maze/Maze.vue
@@ -2,8 +2,8 @@
   <div v-if="!gameState">
     Loading..... A css loader would be good
   </div>
-  <div v-else>
-    {{ gameState }}
+  <div v-else class="maze-game-ascii gold">
+    <pre>{{ mapMaze() }}</pre>
   </div>
 </template>
 
@@ -19,11 +19,164 @@ export default {
   },
   methods: {
     async createGameSession() {
+      this.gameViz = {
+        Blocked: "██",
+        Exit: "▒▒",
+        Open: "  ",
+        Player: "⋐⋑",
+      };
       //this.gameState = (await this.$axios.get("/api/game/maze")).data;
-      this.gameState = (await this.$axios.get(
-        "https://dsdsd.free.beeceptor.com/api/game/maze",
-      )).data;
+      this.gameState = {
+        player: { x: 0, y: 0 },
+        exit: { x: 9, y: 0 },
+        map: [
+          [
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+          ],
+          [
+            "Open",
+            "Open",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Open",
+          ],
+        ],
+      };
+    },
+    mapMaze() {
+      const map = this.gameState.map;
+      const [playerX, playerY] = [
+        this.gameState.player.x,
+        this.gameState.player.y,
+      ];
+      const [exitX, exitY] = [this.gameState.exit.x, this.gameState.exit.y];
+      map[playerY][playerX] = "Player";
+      map[exitY][exitX] = "Exit";
+
+      return map.map(x => x.map(y => this.gameViz[y]).join("")).join("\n");
     },
   },
 };
 </script>
+
+<style scoped>
+.maze-game-ascii {
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+}
+.maze-game-ascii > pre {
+  font-size: 2.25vw;
+  word-spacing: 0;
+}
+</style>
