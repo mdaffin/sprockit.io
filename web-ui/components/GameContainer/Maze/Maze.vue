cd82ce5c75db8b802284252db721a2bc828e7e39
fsternder <florian@sterneder.net>
2019-10-20 15:39:14 +0200

    display (hard-coded) map

--- web-ui/components/GameContainer/Maze/Maze.vue
+++ web-ui/components/GameContainer/Maze/Maze.vue
@@ -2,8 +2,10 @@
   <div v-if="!gameState">
     Loading..... A css loader would be good
   </div>
-  <div v-else>
-    {{ gameState }}
+  <div v-else class="maze-game-ascii gold">
+    <pre v-for="row in mapMaze()">
+      {{ row }}
+    </pre>
   </div>
 </template>
 
@@ -19,11 +21,164 @@ export default {
   },
   methods: {
     async createGameSession() {
+      this.gameViz = {
+        Player: "@",
+        Open: "▁",
+        Blocked: "▒",
+        Exit: "█",
+      };
       //this.gameState = (await this.$axios.get("/api/game/maze")).data;
-      this.gameState = (await this.$axios.get(
-        "https://dsdsd.free.beeceptor.com/api/game/maze",
-      )).data;
+      this.gameState = {
+        player: { x: 0, y: 0 },
+        exit: { x: 9, y: 0 },
+        map: [
+          [
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+            "Open",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+          [
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+            "Blocked",
+          ],
+        ],
+      };
+    },
+    mapMaze() {
+      const map = this.gameState.map;
+      const [playerX, playerY] = [
+        this.gameState.player.x,
+        this.gameState.player.y,
+      ];
+      const [exitX, exitY] = [this.gameState.exit.x, this.gameState.exit.y];
+      map[playerY][playerX] = "Player";
+      map[exitX][exitY] = "Exit";
+
+      return map.map(x => x.map(y => this.gameViz[y]).join(" "));
     },
   },
 };
 </script>
+
+<style scoped>
+.maze-game-ascii {
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+}
+.maze-game-ascii > pre {
+  font-size: 2.25vw;
+  margin: -5px 0;
+}
+</style>
