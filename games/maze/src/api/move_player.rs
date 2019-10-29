7c655e88ab99d601d30a679269167ae8bb56020c
Michael Daffin <michael@daffin.io>
2019-10-29 13:42:42 +0000

    Refactors the player move tests

--- games/maze/src/api/move_player.rs
+++ games/maze/src/api/move_player.rs
@@ -15,7 +15,7 @@ pub fn move_player(
         .ok_or(ServiceError::SessionNotFound)?;
 
     maze.move_player(*direction);
-    Ok(HttpResponse::Ok().body(""))
+    Ok(HttpResponse::Ok().body("{}"))
 }
 #[cfg(test)]
 mod tests {
@@ -23,69 +23,14 @@ mod tests {
     use crate::maze::tests::maze_from_slice_with_player_at;
     use crate::maze::{Direction, Tile};
     use actix_web::{test, web, App};
+    use lazy_static::lazy_static;
     use std::collections::HashMap;
     use std::sync::Mutex;
 
-    mod move_player_with {
+    mod when_player_moves_in {
         use super::*;
-
-        #[test]
-        fn open_direction_up() {
-            test_open(Direction::Up, 1, 0);
-        }
-        #[test]
-        fn open_direction_down() {
-            test_open(Direction::Down, 1, 2);
-        }
-        #[test]
-        fn open_direction_left() {
-            test_open(Direction::Left, 0, 1);
-        }
-        #[test]
-        fn open_direction_right() {
-            test_open(Direction::Right, 2, 1);
-        }
-
-        #[test]
-        fn blocked_direction_up() {
-            test_blocked(Direction::Up);
-        }
-        #[test]
-        fn blocked_direction_down() {
-            test_blocked(Direction::Down);
-        }
-        #[test]
-        fn blocked_direction_left() {
-            test_blocked(Direction::Left);
-        }
-        #[test]
-        fn blocked_direction_right() {
-            test_blocked(Direction::Right);
-        }
-
-        #[test]
-        fn edge_direction_up() {
-            test_edge(Direction::Up);
-        }
-        #[test]
-        fn edge_direction_down() {
-            test_edge(Direction::Down);
-        }
-        #[test]
-        fn edge_direction_left() {
-            test_edge(Direction::Left);
-        }
-        #[test]
-        fn edge_direction_right() {
-            test_edge(Direction::Right);
-        }
-
-        fn test_open(direction: Direction, x: usize, y: usize) {
-            test(direction, 1, 1, x, y, &[Tile::open(); 3 * 3])
-        }
-
-        fn test_blocked(direction: Direction) {
-            let map = &[
+        lazy_static! {
+            static ref BLOCKED_MAP: [Tile; 9] = [
                 Tile::blocked(),
                 Tile::blocked(),
                 Tile::blocked(),
@@ -96,14 +41,60 @@ mod tests {
                 Tile::blocked(),
                 Tile::blocked(),
             ];
-            test(direction, 1, 1, 1, 1, map)
         }
 
-        fn test_edge(direction: Direction) {
-            test(direction, 0, 0, 0, 0, &[Tile::open()])
+        #[test]
+        fn open_direction_up_player_is_moved() {
+            test_player_moves(Direction::Up, 1, 1, 1, 0, &[Tile::open(); 3 * 3]);
+        }
+        #[test]
+        fn open_direction_down_player_is_moved() {
+            test_player_moves(Direction::Down, 1, 1, 1, 2, &[Tile::open(); 3 * 3]);
+        }
+        #[test]
+        fn open_direction_left_player_is_moved() {
+            test_player_moves(Direction::Left, 1, 1, 0, 1, &[Tile::open(); 3 * 3]);
+        }
+        #[test]
+        fn open_direction_right_player_is_moved() {
+            test_player_moves(Direction::Right, 1, 1, 2, 1, &[Tile::open(); 3 * 3]);
         }
 
-        fn test(
+        #[test]
+        fn blocked_direction_up_player_is_moved() {
+            test_player_moves(Direction::Up, 1, 1, 1, 1, &BLOCKED_MAP[..]);
+        }
+        #[test]
+        fn blocked_direction_down_player_is_moved() {
+            test_player_moves(Direction::Down, 1, 1, 1, 1, &BLOCKED_MAP[..]);
+        }
+        #[test]
+        fn blocked_direction_left_player_is_moved() {
+            test_player_moves(Direction::Left, 1, 1, 1, 1, &BLOCKED_MAP[..]);
+        }
+        #[test]
+        fn blocked_direction_right_player_is_moved() {
+            test_player_moves(Direction::Right, 1, 1, 1, 1, &BLOCKED_MAP[..]);
+        }
+
+        #[test]
+        fn edge_direction_up_player_is_moved() {
+            test_player_moves(Direction::Up, 0, 0, 0, 0, &[Tile::open()]);
+        }
+        #[test]
+        fn edge_direction_down_player_is_moved() {
+            test_player_moves(Direction::Down, 0, 0, 0, 0, &[Tile::open()]);
+        }
+        #[test]
+        fn edge_direction_left_player_is_moved() {
+            test_player_moves(Direction::Left, 0, 0, 0, 0, &[Tile::open()]);
+        }
+        #[test]
+        fn edge_direction_right_player_is_moved() {
+            test_player_moves(Direction::Right, 0, 0, 0, 0, &[Tile::open()]);
+        }
+
+        fn test_player_moves(
             direction: Direction,
             start_x: usize,
             start_y: usize,
@@ -111,6 +102,23 @@ mod tests {
             end_y: usize,
             map: &[Tile],
         ) {
+            let (sessions, token, _) = run_test(direction, start_x, start_y, map);
+
+            let player = {
+                let sessions = sessions.lock().unwrap();
+                (*sessions).get(&token).unwrap().player()
+            };
+
+            assert_eq!(player.x, end_x);
+            assert_eq!(player.y, end_y);
+        }
+
+        fn run_test(
+            direction: Direction,
+            start_x: usize,
+            start_y: usize,
+            map: &[Tile],
+        ) -> (Sessions, SessionToken, serde_json::Value) {
             let sessions: Sessions = web::Data::new(Mutex::new(HashMap::new()));
             let token = SessionToken::new();
             let maze = maze_from_slice_with_player_at(start_x, start_y, map);
@@ -127,15 +135,9 @@ mod tests {
                 .header("X-TOKEN", token.to_string())
                 .to_request();
 
-            dbg!(test::call_service(&mut app, req));
+            let response: serde_json::Value = dbg!(test::read_response_json(&mut app, req));
 
-            let player = {
-                let sessions = sessions.lock().unwrap();
-                (*sessions).get(&token).unwrap().player()
-            };
-
-            assert_eq!(player.x, end_x);
-            assert_eq!(player.y, end_y);
+            (sessions, token, response)
         }
     }
 }
