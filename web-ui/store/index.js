ee9aa77c9d4a0ac27356207f04993c8c65475194
Michael Daffin <michael@daffin.io>
2019-11-03 19:25:48 +0000

    Moves the code to the vuex store (#83)

--- web-ui/store/index.js
+++ web-ui/store/index.js
@@ -1,12 +1,22 @@
+const DEFAULT_SCRIPT = `const a = 10;
+const b = 20;
+console.log(a + b);
+console.log('A String');
+`;
+
 export const state = () => ({
   maze: Array.from({ length: 10 }, () =>
     Array.from({ length: 10 }, () => "hidden"),
   ),
+  script: DEFAULT_SCRIPT,
 });
 
 export const mutations = {
   setMaze(state, maze) {
     state.maze = maze;
+  },
+  setScript(state, script) {
+    state.script = script;
   },
 };
 
--- web-ui/tests/utils.js
+++ web-ui/tests/utils.js
@@ -1,16 +1,19 @@
 import Vuex from "vuex";
 import { createLocalVue } from "@vue/test-utils";
-import { mutations } from "@/store/console";
+import { mutations as consoleMutations } from "@/store/console";
+import { mutations } from "@/store";
 
-export const createStore = lines => {
+export const createStore = ({ script, lines }) => {
   const localVue = createLocalVue();
   localVue.use(Vuex);
   const store = new Vuex.Store({
+    state: { script },
+    mutations,
     modules: {
       console: {
         namespaced: true,
         state: { lines },
-        mutations,
+        mutations: consoleMutations,
       },
     },
   });
