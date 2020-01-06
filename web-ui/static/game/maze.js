8a6175f54a61336819c94be7af99fca846e733ed
fsterneder <16085695+fsterneder@users.noreply.github.com>
2020-01-06 23:24:01 +0100

    Maze.js returns directions and slightly adapted default script (#115)

--- web-ui/static/game/maze.js
+++ web-ui/static/game/maze.js
@@ -3,46 +3,47 @@ async function sendRequest(url, token, method) {
   const response = await fetch(`/api/game/maze/${url}`, { method, headers });
 
   if (!response.ok) {
-    throw new Error(
-      `bad response: ${response.status} ${
-        response.statusText
-      } ${await response.text()}`,
-    );
-  }
-
-  if (response.status == 204) {
+    console.log("Error: " + (await response.text()));
+  } else if (response.status == 204) {
     return;
+  } else {
+    return response.json();
   }
-
-  return response.json();
 }
 
 const post = async (url, token) => sendRequest(url, token, "POST");
 const get = async (url, token) => sendRequest(url, token, "GET");
 
 export class Maze {
+  async moveTemplate(arg) {
+    const map = await post(`move/${arg}`, this.token);
+    parent.updateMaze(await get("map", this.token));
+    return map;
+  }
+
   async start() {
     this.token = (await post("start")).token;
     parent.updateMaze(await get("map", this.token));
   }
 
+  async directions() {
+    const map = await get("move", this.token);
+    return map;
+  }
+
   async moveUp() {
-    await post("move/up", this.token);
-    parent.updateMaze(await get("map", this.token));
+    return this.moveTemplate("up");
   }
 
   async moveDown() {
-    await post("move/down", this.token);
-    parent.updateMaze(await get("map", this.token));
+    return this.moveTemplate("down");
   }
 
   async moveLeft() {
-    await post("move/left", this.token);
-    parent.updateMaze(await get("map", this.token));
+    return this.moveTemplate("left");
   }
 
   async moveRight() {
-    await post("move/right", this.token);
-    parent.updateMaze(await get("map", this.token));
+    return this.moveTemplate("right");
   }
 }
