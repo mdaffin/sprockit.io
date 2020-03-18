f7f17125dbd723c4157e134ac3d6f69d0fe130d9
Michael Daffin <michael@daffin.io>
2020-03-18 09:21:21 +0000

    Updates packages and fixes tests that broke due to it

--- web-ui/tests/jest.setup.js
+++ web-ui/tests/jest.setup.js
@@ -1,6 +1,6 @@
-import VueTestUtils from "@vue/test-utils";
+import { config } from "@vue/test-utils";
 
 // Mock Nuxt components
-VueTestUtils.config.stubs["nuxt-link"] = "<a><slot /></a>";
-VueTestUtils.config.stubs["client-only"] = "<span><slot /></span>";
-VueTestUtils.config.stubs["codemirror"] = "<span><slot /></span>";
+config.stubs["nuxt-link"] = "<a><slot /></a>";
+config.stubs["client-only"] = "<span><slot /></span>";
+config.stubs["codemirror"] = "<span><slot /></span>";
